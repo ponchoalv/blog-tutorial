3a481b26abc6b3847c71bc2f56371fe9f317f9f1
Alfonso Fernando Álvarez <alfonso@linux-29bx.(none)>
2011-12-02 00:23:07 -0300

    version final del tutorial

--- app.js
+++ app.js
@@ -5,6 +5,8 @@
 var express = require('express'),
 routes = require('./routes');
 
+var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
+
 var app = module.exports = express.createServer();
 
 // Configuration
@@ -31,8 +33,61 @@ app.configure('production', function() {
 	app.use(express.errorHandler());
 });
 
+var articleProvider = new ArticleProvider('localhost', 27017);
+
 // Routes
-app.get('/', routes.index);
+//app.get('/', routes.index);
+app.get('/', function(req, res) {
+	articleProvider.findAll(function(error, docs) {
+		res.render('index.jade', {
+			locals: {
+				title: 'blog',
+				articles: docs
+			}
+		});
+	});
+});
+
+app.get('/blog/new', function(req, res) {
+	res.render('blog_new.jade', {
+		locals: {
+			title: 'New Post'
+		}
+	});
+});
+
+app.post('/blog/new', function(req, res) {
+	articleProvider.save({
+		title: req.param('title'),
+		body: req.param('body')
+	},
+	function(error, docs) {
+		res.redirect('/');
+	});
+});
+
+app.get('/blog/:id', function(req, res) {
+	articleProvider.findById(req.params.id, function(error, article) {
+		res.render('blog_show.jade', {
+			locals: {
+				title: article.title,
+				article: article
+			}
+		});
+
+	});
+});
+
+app.post('/blog/addComment', function(req, res) {
+	articleProvider.addCommentToArticle(req.param('_id'), {
+		person: req.param('person'),
+		comment: req.param('comment'),
+		created_at: new Date()
+	},
+	function(errors, docs) {
+		res.redirect('/blog/' + req.param('_id'));
+	});
+});
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
